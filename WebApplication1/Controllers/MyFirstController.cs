a901bb9d4b0c95bfa3755798756a82a4254644dd
Ozodbek <olimjanovozodbek32@gmail.com>
2024-02-14 11:00:55 +0500

    Add project files.

---
+++ WebApplication1/Controllers/MyFirstController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using WebApplication1.NewFolder;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller],[action]")]
+    [ApiController]
+    public class MyFirstController : ControllerBase
+    {
+        private readonly string connectionString = "Server=127.0.0.1;Port=5432;Database=MyData;User Id=postgres;" +
+            "Password=admin;";
+        public string TableName = "Data1";
+        [HttpGet]
+        public List<MyModel> Get()
+        {
+            List<MyModel> myModels = new List<MyModel>();
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = $"select * from {TableName};";
+                NpgsqlCommand command = new NpgsqlCommand(query, connection);
+
+                NpgsqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    myModels.Add(new MyModel
+                    {
+                        Id = (int)reader["id"],
+                        Name = (string)reader["name"]
+                    });
+                }
+                reader.Close();
+                return myModels;
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/MyModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.NewFolder
+{
+    public class MyModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
